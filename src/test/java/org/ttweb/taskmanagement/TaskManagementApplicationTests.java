26fc8de7a2bd687374db96324bb6a22f7ff2750b
shagy <shuhratberdiyev@gmail.com>
2021-07-21 20:47:23 +0500

    create backend scaffold

---
+++ src/test/java/org/ttweb/taskmanagement/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ttweb.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
