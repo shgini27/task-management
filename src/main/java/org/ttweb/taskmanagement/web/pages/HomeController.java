73c76e9bee4a3eae97b94d4e4cb995c0d45cf722
shagy <shuhratberdiyev@gmail.com>
2021-07-23 00:15:50 +0500

    add /register to request mapping

--- src/main/java/org/ttweb/taskmanagement/web/pages/HomeController.java
+++ src/main/java/org/ttweb/taskmanagement/web/pages/HomeController.java
@@ -1,11 +1,11 @@
-package org.ttweb.taskmanagement.web;
+package org.ttweb.taskmanagement.web.pages;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping(value = {"/", "/login"})
+    @GetMapping(value = {"/", "/login", "/register"})
     public String home(){
         return "index";
     }
