c03cad32547ac38e04dcb98524e9469cbc4e03f6
shagy <shuhratberdiyev@gmail.com>
2021-07-26 16:52:48 +0500

    implement PasswordEncryptorDelegate

--- src/main/java/org/ttweb/taskmanagement/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/org/ttweb/taskmanagement/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,12 +1,20 @@
 package org.ttweb.taskmanagement.domain.common.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public PasswordEncryptorDelegator(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public String encrypt(String rawPassword) {
-        //TODO: implement password encryption
-        return rawPassword;
+        return passwordEncoder.encode(rawPassword);
     }
 }
